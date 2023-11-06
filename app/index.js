fcc631739385b6d9c762839fa104df14ec29191f
mixxen <alexander.cabello@gmail.com>
2023-11-06 09:48:02 -1000

    Update to SatSim v0.2.0.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
-import { Universe, createViewer, fetchTle } from "satsim";
-import { Math as CMath, JulianDate, viewerCesiumInspectorMixin, ClockRange, ClockStep, defined, Color } from "cesium";
+import { Universe, createViewer, fetchTle, southEastZenithToAzEl } from "satsim";
+import { Math as CMath, JulianDate, viewerCesiumInspectorMixin, ClockRange, ClockStep, defined, Color, Cartesian3 } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "./index.css";
 
@@ -26,16 +26,26 @@ viewer.extend(viewerCesiumInspectorMixin);
 //////////////////////
 // Main application //
 //////////////////////
-loadSatellites('assets/celestrak_sat_elem.txt');
-loadSensors('assets/sites.json');
+loadSensors('assets/sites.json').then(() => {
+  loadSatellites('assets/celestrak_sat_elem.txt').then(() => {
 
-const lastTrackTime = JulianDate.clone(start);
-viewer.scene.preUpdate.addEventListener((scene, time) => {
-  if(Math.abs(JulianDate.secondsDifference(lastTrackTime, time)) > 15) {
-    const gimbals = universe.gimbals;
-    randomTrack(gimbals[Math.floor(Math.random() * gimbals.length)], time);
-    JulianDate.clone(time, lastTrackTime);
-  }
+    // track something    
+    universe.update(start);
+    universe._observatories.forEach((o) => {
+      randomTrack(universe, o, start);
+    });
+
+    // random track
+    const lastTrackTime = new JulianDate();
+    viewer.scene.preUpdate.addEventListener((scene, time) => {
+      if(Math.abs(JulianDate.secondsDifference(lastTrackTime, time)) > 15) {
+        const observatories = universe._observatories;
+        randomTrack(universe, observatories[Math.floor(Math.random() * observatories.length)], time);
+        JulianDate.clone(time, lastTrackTime);   
+      }
+    });
+    
+  });
 });
 
 
@@ -43,8 +53,10 @@ viewer.scene.preUpdate.addEventListener((scene, time) => {
 // Helper functions //
 //////////////////////
 async function loadSatellites(url) {
-  fetchTle(url, 3, (line1, line2, line3) => {
-    const satellite = universe.addSGP4Satellite(line2, line3, 'nadir', line1, true);
+  await fetchTle(url, 3, (line1, line2, line3) => {
+    if(universe.hasObject(line1)) //skip duplicates
+      return;
+    const satellite = universe.addSGP4Satellite(line1, line2, line3, 'nadir', true);
     const description = `<div><b>${line1}</b><br><br>${line2}<br>${line3}<br><br>Period: ${(satellite.period / 60).toFixed(2)} min<br><br></div>`;
     const color = new Color.fromRandom({alpha: 1.0});
     viewer.addObjectVisualizer(satellite, description, {
@@ -52,7 +64,7 @@ async function loadSatellites(url) {
         show: false,
         leadTime: satellite.period / 2,
         trailTime: satellite.period / 2,
-        resolution: satellite.period / 300,
+        resolution: satellite.period / (500 / (1 - satellite.eccentricity)), // make eliptical orbits look better
         material: color,
         width: 1      
       },
@@ -73,29 +85,32 @@ async function loadSensors(url) {
     const description = `<div><b>${o.site.name}</b><br><br>Latitude: ${o.site.latitude} deg<br>Longitude: ${o.site.longitude} deg<br>Altitude: ${o.site.altitude} m<br><br></div>`;
     viewer.addObservatoryVisualizer(o, description);
     o.gimbal.trackMode = 'rate';
-    randomTrack(o.gimbal, viewer.clock.currentTime);
   }
 }
 
-function randomTrack(gimbal, time) {
-  let maxIterations = 500;
-  let keys = Object.keys(universe.objects);
-
-  showTrackedPath(gimbal, false);
-
+function randomTrack(universe, observatory, time, maxIterations=500) {
+  showTrackedPath(observatory.gimbal, false);
+  const numTrackables = universe._trackables.length;
+  if(numTrackables === 0) {
+    return;
+  }
+  const localPos = new Cartesian3();
   while(true) {
-    let ob = universe._objects[keys[Math.floor(Math.random() * keys.length)]];
+    let ob = universe._trackables[Math.floor(Math.random() * numTrackables)];
     if(defined(ob)) {
-      gimbal.trackObject = ob;
-      gimbal.update(time, universe);
-      if(ob.period < 2000 * 60 && gimbal.el > 30 || maxIterations < 0) {
+      observatory.site.transformPointFromWorld(ob.worldPosition, localPos);
+      let [az, el, r] = southEastZenithToAzEl(localPos)
+      if(ob.period < 2000 * 60 && el > 30) {
+        observatory.gimbal.trackObject = ob;
+        observatory.gimbal.update(time, universe);
+        break;
+      } else if(maxIterations < 0) {
         break;
       }
     }
     maxIterations--;
   }
-
-  showTrackedPath(gimbal, true);
+  showTrackedPath(observatory.gimbal, true);
 }
 
 function showTrackedPath(gimbal, show) {
